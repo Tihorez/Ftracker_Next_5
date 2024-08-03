6fcbba7bf48bb2f7249f9029e8d68d095a2034b6
Tikhon Svintsitskiy <dragonbal-3@protonmail.ch>
2024-08-03 19:09:14 +0300

    change calories

--- main.go
+++ main.go
@@ -121,8 +121,9 @@ type Walking struct {
 // * 0.029 * вес_спортсмена_в_кг) * время_тренировки_в_часах * мин_в_ч)
 // Это переопределенный метод Calories() из Training.
 func (w Walking) Calories() float64 {
-	return ((CaloriesWeightMultiplier*w.Weight + (math.Pow(w.meanSpeed()*KmHInMsec, 2)/
-		w.Height)*CaloriesSpeedHeightMultiplier*w.Weight) * w.Duration.Hours() * MinInHours)
+	speedMsec := w.meanSpeed() * KmHInMsec
+	heightM := w.Height / CmInM
+	return ((CaloriesWeightMultiplier*w.Weight + (math.Pow(speedMsec, 2)/heightM)*CaloriesSpeedHeightMultiplier*w.Weight) * w.Duration.Hours() * MinInHours)
 }
 
 // TrainingInfo возвращает структуру InfoMessage с информацией о проведенной тренировке.
